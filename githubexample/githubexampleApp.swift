08c5c1cf8d40539b84c0a045b95676094b23c520
Saloni <salonipanchamia29@gmail.com>
2023-07-14 10:17:40 -0400

    Initial Commit

---
+++ githubexample/githubexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubexampleApp.swift
+//  githubexample
+//
+//  Created by Saloni Panchamia on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct githubexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
